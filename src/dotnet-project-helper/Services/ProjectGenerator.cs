71fde622e1ec3dedf9fa42b156e0e2ff573fefbc
Mario Klawuhn <mario.klawuhn@gmx.de>
2020-01-13 14:57:04 +0100

    app automatically adds all files and commits them to the repo

--- src/dotnet-project-helper/Services/ProjectGenerator.cs
+++ src/dotnet-project-helper/Services/ProjectGenerator.cs
@@ -19,25 +19,52 @@ namespace dotnet_project_helper.Services
 
             commands = new List<Command>();
 
+            CreateRawProject(commands);
+
+            if (parser.createTestProject)
+                AddTestProject(commands);
+
+            commands.Add(new Command { Value = $"dotnet sln {parser.getFullPath}/{parser.getAppName}.sln  add {parser.getFullPath}/src/{parser.getAppName}*" });
+
+            if (parser.createGitRepo)
+                AddGitRepo(commands);
+        }
+
+        private void AddGitRepo(List<Command> commands)
+        {
+            commands.Add(new Command { Value = $"dotnet new gitignore -o {parser.getFullPath}" });
+
+            commands.Add(new Command { Value = $"git init {this.parser.getFullPath}" });
+
+            commands.Add(new Command { Value = $"git --git-dir={parser.getFullPath}/.git/  add . " });
+
+            commands.Add(new Command { Value = $"git --git-dir={parser.getFullPath}/.git/ commit -m 'First commit'" });
+
+        }
+
+        private void AddTestProject(List<Command> commands)
+        {
+            commands.Add(new Command { Value = $"dotnet new xunit -o {parser.getFullPath}/src/{parser.getAppName}.test" });
+        }
+
+        private void CreateRawProject(List<Command> commands)
+        {
             commands.Add(new Command { Value = $"mkdir {this.parser.getFullPath}" });
 
             commands.Add(new Command { Value = $"mkdir {this.parser.getFullPath}/src" });
 
-            commands.Add(new Command { Value = $"dotnet new {this.parser.getAppType} -o {this.parser.getFullPath}/src/test" });
+            commands.Add(new Command { Value = $"dotnet new {this.parser.getAppType}  -o {this.parser.getFullPath}/src/{parser.getAppName}" });
 
-            if (parser.createGitRepo)
-            {
-                commands.Add(new Command { Value = $"dotnet new gitignore -o {parser.getFullPath}" });
-
-                commands.Add(new Command { Value = $"git init {this.parser.getFullPath}" });
-            }
+            commands.Add(new Command { Value = $"dotnet new sln -n {parser.getAppName} -o {this.parser.getFullPath}" });
         }
 
         public async Task Create()
         {
             foreach (Command command in commands)
             {
-                await executor.Execute(command);
+                var result = await executor.Execute(command);
+
+                System.Console.WriteLine(result.Result);
             }
         }
     }

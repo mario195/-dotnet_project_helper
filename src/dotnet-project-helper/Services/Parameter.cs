75634de888ec5d7e7f41b2adaeb7d831aa7a6067
Mario Klawuhn <mario.klawuhn@gmx.de>
2020-01-13 11:11:26 +0100

    Wrapper class with contains the application parameter

---
+++ src/dotnet-project-helper/Services/Parameter.cs
@@ -0,0 +1,13 @@
+namespace dotnet_project_helper.Services
+{
+    public class Parameter
+    {
+        public string ApplicationType { get; set; }
+
+        public bool GitInit { get; set; }
+
+        public bool TestProject { get; set; }
+        
+        public string Path { get; set; }
+    }
+}

1fe32af212ce7cb3a6764d11c98d790db66dc78d
Mario Klawuhn <mario.klawuhn@gmx.de>
2020-01-14 15:18:14 +0100

    Display Function in Parameter class

--- src/dotnet-project-helper.lib/Services/Parameter.cs
+++ src/dotnet-project-helper.lib/Services/Parameter.cs
@@ -7,7 +7,20 @@ namespace dotnet_project_helper.lib.Services
         public bool GitInit { get; set; }
 
         public bool TestProject { get; set; }
-        
+
         public string Path { get; set; }
+
+        public string Name { get; set; }
+
+        public void DisplayParameter()
+        {
+            System.Console.WriteLine("Generating Project with the following Parameter:");
+            System.Console.WriteLine("-------------------------------------------------");
+            System.Console.WriteLine($"Application name: {this.Name}");
+            System.Console.WriteLine($"Application path: {this.Path}");
+            System.Console.WriteLine($"Application type: {this.ApplicationType}");
+            System.Console.WriteLine($"Generate Test Project? {this.TestProject}");
+            System.Console.WriteLine($"Generate Git Repo? {this.GitInit}");
+        }
     }
 }

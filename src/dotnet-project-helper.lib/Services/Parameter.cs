0693f47f483090325404bb790ba4d6549b76b46e
Mario Klawuhn <mario.klawuhn@gmx.de>
2020-01-14 15:33:18 +0100

    new prop in parameter class for verbose output

--- src/dotnet-project-helper.lib/Services/Parameter.cs
+++ src/dotnet-project-helper.lib/Services/Parameter.cs
@@ -8,6 +8,8 @@ namespace dotnet_project_helper.lib.Services
 
         public bool TestProject { get; set; }
 
+        public bool VerboseOutput { get; set; }
+
         public string Path { get; set; }
 
         public string Name { get; set; }
@@ -15,12 +17,14 @@ namespace dotnet_project_helper.lib.Services
         public void DisplayParameter()
         {
             System.Console.WriteLine("Generating Project with the following Parameter:");
-            System.Console.WriteLine("-------------------------------------------------");
+            System.Console.WriteLine("------------------------------------------------");
             System.Console.WriteLine($"Application name: {this.Name}");
             System.Console.WriteLine($"Application path: {this.Path}");
             System.Console.WriteLine($"Application type: {this.ApplicationType}");
-            System.Console.WriteLine($"Generate Test Project? {this.TestProject}");
-            System.Console.WriteLine($"Generate Git Repo? {this.GitInit}");
+            System.Console.WriteLine($"Generate Test Project? {(this.TestProject ? "Yes" : "No")}");
+            System.Console.WriteLine($"Generate Git Repo? {(this.GitInit ? "Yes" : "No")}");
+            System.Console.WriteLine($"Verbose output? {(this.VerboseOutput ? "On" : "Off")}");
+            System.Console.WriteLine("------------------------------------------------");
         }
     }
 }

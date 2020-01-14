f44cd2ad4afe24de6aba8e5ab39d58dac7faff51
Mario Klawuhn <mario.klawuhn@gmx.de>
2020-01-14 10:57:49 +0100

    Test cases for CliArgsParser

--- src/dotnet-project-helper.test/CliArgsParser.test.cs
+++ src/dotnet-project-helper.test/CliArgsParser.test.cs
@@ -1,14 +1,77 @@
 using System;
 using Xunit;
+using dotnet_project_helper.lib.Services;
 
 namespace dotnet_project_helper.test
 {
-    public class CliArgsParser
+    public class CliArgsParserTest
     {
-        [Fact]
-        public void Test1()
-        {
+        string[] args;
 
+        CliArgsParser parser;
+
+        public CliArgsParserTest()
+        {
+            parser = new CliArgsParser();
+
+            args = new[]{
+                "-g",
+                "-t",
+                "-p",
+                "~/test",
+                "-a",
+                "console",
+                "-n",
+                "new-app"
+             };
+
+            parser.Parse(args);
+        }
+
+        [Fact]
+        public void ShouldReturnAppname()
+        {
+            var appname = args[7];
+
+            var parsed_appname = parser.getAppName;
+
+            Assert.Equal(parsed_appname, appname);
+        }
+
+        [Fact]
+        public void ShouldReturnFullPath()
+        {
+            var path = args[3];
+
+            var parsed_path = parser.getFullPath;
+
+            Assert.Equal(parsed_path, path);
+        }
+
+        [Fact]
+        public void ShouldReturnApptype()
+        {
+            var apptype = args[5];
+
+            var parsed_apptype = parser.getAppType;
+
+            Assert.Equal(parsed_apptype, apptype);
+        }
+
+        [Fact]
+        public void TrueIfRepoShouldBeCreated()
+        {
+            var parsed_createRepo = parser.shouldCreateGitRepo;
+
+            Assert.True(parsed_createRepo);
+        }
+
+        [Fact]
+        public void TrueIfTestProjectShouldBeCreated()
+        {
+            var parsed_createTestProject = parser.shouldCreateTestProject;
+
+            Assert.True(parsed_createTestProject);
         }
     }
 }
